3341e785f75543d1125268ad9c68bddfd0a45a33
FadwaFouad <fadoalqasem@gmail.com>
2022-11-08 09:49:04 +0200

    track life cycle

---
+++ app/src/main/java/com/example/rolethedice/AppLifecycleObserver.kt
@@ -0,0 +1,38 @@
+package com.example.rolethedice
+
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
+class AppLifecycleObserver : DefaultLifecycleObserver {
+
+    override fun onCreate(owner: LifecycleOwner) {
+        Log.i(LOG_TAG, "on create")
+        super.onCreate(owner)
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        Log.i(LOG_TAG, "on start")
+        super.onStart(owner)
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        Log.i(LOG_TAG, "on resume")
+        super.onResume(owner)
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        Log.i(LOG_TAG, "on pause")
+        super.onPause(owner)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        Log.i(LOG_TAG, "on stop")
+        super.onStop(owner)
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        Log.i(LOG_TAG, "on destroy")
+        super.onDestroy(owner)
+    }
+}
---
+++ app/src/main/java/com/example/rolethedice/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.rolethedice
+
+const val LOG_TAG = "role_the_dice"
